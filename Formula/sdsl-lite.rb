d96cf918398f12dff6ab94abf520fce54f918557
Mikhail Karasikov <karasikov@users.noreply.github.com>
2018-10-18 00:08:15 +0200

    Update sdsl-lite.rb

--- Formula/sdsl-lite.rb
+++ Formula/sdsl-lite.rb
@@ -6,7 +6,10 @@ class SdslLite < Formula
   head "https://github.com/simongog/sdsl-lite", :branch => "master"
 
   # Use git to get submodules.
-  url "https://github.com/simongog/sdsl-lite.git"
+  url "https://github.com/simongog/sdsl-lite.git",
+    :revision => "0546faf0552142f06ff4b201b671a5769dd007ad"
+    :tag => "v2.1.1"
+  revision 2
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
