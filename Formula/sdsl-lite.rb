c96b8c145dab1f4e16c3055a61ca7d9b0b53e8ba
Mikhail Karasikov <karasikov@users.noreply.github.com>
2018-10-17 23:49:45 +0200

    Update sdsl-lite.rb

--- Formula/sdsl-lite.rb
+++ Formula/sdsl-lite.rb
@@ -5,7 +5,7 @@ class SdslLite < Formula
 
   # Use git to get submodules.
   url "https://github.com/simongog/sdsl-lite.git",
-    :revision => "0546faf0552142f06ff4b201b671a5769dd007ad",
+    :revision => "ddb0fbbc33bb183baa616f17eb48e261ac2a3672",
     :tag => "v2.1.1"
   revision 2
 
